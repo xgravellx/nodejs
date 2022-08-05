e56348ea8315c98d0fb952ec90d4e497d199e7ca
Hazal <88984994+xgravellx@users.noreply.github.com>
2022-08-05 19:09:11 +0300

    fourth commit

---
+++ npm/npm-commands.js
@@ -0,0 +1,16 @@
+// npm init ---> npm kullanmamız için package.json yapısını oluşturur. --yes ile tüm sorulara default cevaplar verilir.
+// npm i veya install -g npm@6.14.4 --> global olarak npmyi ilgili sürüm olarak yükler.
+// node_modules dosyasını sildikten sonra npm install diyerek yükleme yapabiliriz
+// o proje için bağımlılıkları gösterir --> npm list --depth=0
+// npm list --depth=1 dersek 1. kademeyi kullanır.
+// npm view mongoose paket açıklamaları
+
+// "express": "^4.18.1", //Major, Minor, Patch 4.18.1
+
+// npm un eslint ile packeti silebiliriz
+
+// npm i eslint --save-dev -->Aynı İşlev<-- npm i eslint -D
+
+
+
+console.log('Hello Hazel');
