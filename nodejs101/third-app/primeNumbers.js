e99c01d7ed2d8d53854424092f7e7890c3fd7600
Hazal <88984994+xgravellx@users.noreply.github.com>
2022-10-25 12:23:52 +0300

    first commit

---
+++ nodejs101/third-app/primeNumbers.js
@@ -0,0 +1,20 @@
+// 1 e ve kendisine bölünecek, 1 den büyük pozitif tam sayı olacak
+
+const arguments = process.argv.slice(2);
+
+const showPrimeNumber = (lowNumber, highNumber) => {
+    for (let i = lowNumber; i <= highNumber; i++) {
+        let isPrime = true;
+        for (let j = 2; j <= i; j++) {
+            if ( i % j === 0 && j !== i) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            console.log(i);
+        }
+    }
+}
+
+showPrimeNumber(arguments[0] *1 , arguments[1] *1);
+console.log(process.argv);
